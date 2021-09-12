e5b5fa25a22db12f24510ed2f7a2d60710ff5415
Jaqueline Ferreira de Brito <jaquedebrito@hotmail.com>
2021-09-12 08:22:19 -0300

    Basic static components

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/styles.css';
+import '.assets/img/d-dark.svg';
 import App from './App';
 
 ReactDOM.render(
